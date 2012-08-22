ba34e16de351548c5881778fe39959ff55882339
kojak <tamas.kojedzinszky@gmail.com>
2012-08-22 17:00:20 +0200

    experimenting with parser result

--- js/pdfparser.js
+++ js/pdfparser.js
@@ -1,5 +1,23 @@
 (function ($) {
   $(document).ready(function() {
-    console.log('loaded');
+    console.log('pdfparser loaded...');
   });
+  function pdfparser_show_title() {
+    alert(Drupal.settings.pdfparser.title);
+  }
+  
+  function pdfparser_test() {
+    console.log('test');
+    alert('test');
+  }
+  
+  console.log('asder');
+  
 })(jQuery);
+
+console.log('asder2');
+
+function pdfparser_test2() {
+  console.log('test');
+  alert('test');
+}
