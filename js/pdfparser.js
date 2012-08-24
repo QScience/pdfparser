7a0eb1419a2221132d574f0ea8fa96fd60d557b2
kojak <tamas.kojedzinszky@gmail.com>
2012-08-24 17:56:57 +0200

    improved error handling; pars. server saves multiple public keys; some minor updates

--- js/pdfparser.js
+++ js/pdfparser.js
@@ -5,6 +5,13 @@
   
   $(document).ajaxComplete(function(e, xhr, settings) {
     if (settings.extraData._triggering_element_name == 'field_paper_upload_und_0_upload_button') {
+//      var num = $('#edit-field-paper-upload div.messages').length;
+//      var i = 1;
+//      $('#edit-field-paper-upload div.messages').each(function() {
+//        if (i >= num) return;
+//        $(this).slideUp(1000);
+//        i++;
+//      });
       if (Drupal.settings && Drupal.settings.pdfparser) {
         if (Drupal.settings.pdfparser.title) {
           $('#edit-title').val(Drupal.settings.pdfparser.title);
@@ -20,6 +27,7 @@
       $('#edit-title').val('');
       $('#edit-abstract-und-0-value').val('');
       $('#edit-authorname-und-0-value').val('');
+      $('#edit-field-paper-upload div.messages').slideUp(400);
     }
     
   });
