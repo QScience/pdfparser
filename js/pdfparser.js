60eef78b7acd28c19c20e4d963338e260195b508
kojak <tamas.kojedzinszky@gmail.com>
2012-12-04 12:11:57 +0100

    Authors are added again to the Paper automatically by pdfparser.

--- js/pdfparser.js
+++ js/pdfparser.js
@@ -4,7 +4,9 @@
   });
   
   $(document).ajaxComplete(function(e, xhr, settings) {
+    console.log(settings);
     if (typeof settings.extraData != 'undefined') {
+      var field_num;
       if (settings.extraData._triggering_element_name == 'field_paper_upload_und_0_upload_button') {
   //      var num = $('#edit-field-paper-upload div.messages').length;
   //      var i = 1;
@@ -21,19 +23,32 @@
             $('#edit-abstract-und-0-value').val(Drupal.settings.pdfparser.abstr);
           }
           if (Drupal.settings.pdfparser.authors) {
-            var field_num = $('#author-reference-values tbody input.form-text').length;
+//            console.log(Drupal.settings.pdfparser.authors);
+            field_num = $('#author-reference-values tbody input.form-text').length;
             var authors = Drupal.settings.pdfparser.authors;
             var author_num = authors.length;
-            var i;
-            for (i = 0; i < author_num - field_num; i++) {
+            var i, j;
+//            for (i = 0; i < author_num - field_num; i++) {
+//              $('#edit-author-reference-und-add-more').trigger('mousedown');
+//            }
+            for (i = 0, j = 0; i < author_num; j++) {
+              if (j >= field_num) {
+                break;
+              } else {
+                $('#author-reference-values tbody #edit-author-reference-und-'+ j +'-target-id').val(authors[i]);
+                Drupal.settings.pdfparser.authors.splice(i, 1);
+              }
+            }
+            if (Drupal.settings.pdfparser.authors.length != 0) {
               $('#edit-author-reference-und-add-more').trigger('mousedown');
             }
-            for (i = 0; i < author_num; i++) {
-              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val(authors[i]);
-            }
-            for (i = author_num; i < field_num; i++) {
-              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val('');
-            }
+//            console.log(Drupal.settings.pdfparser.authors);
+//            for (i = 0; i < author_num; i++) {
+//              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val(authors[i]);
+//            }
+//            for (i = author_num; i < field_num; i++) {
+//              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val('');
+//            }
             //$('#edit-authorname-und-0-value').val(Drupal.settings.pdfparser.authors);
           }
         }
@@ -42,6 +57,17 @@
         $('#edit-abstract-und-0-value').val('');
         $('#edit-authorname-und-0-value').val('');
         $('#edit-field-paper-upload div.messages').slideUp(400);
+      } else if (settings.extraData._triggering_element_name == 'author_reference_add_more') {
+        if (Drupal.settings && Drupal.settings.pdfparser) {
+          if (Drupal.settings.pdfparser.authors && Drupal.settings.pdfparser.authors.length > 0) {
+            field_num = $('#author-reference-values tbody input.form-text').length;
+            $('#author-reference-values tbody #edit-author-reference-und-'+ (field_num - 1) +'-target-id').val(Drupal.settings.pdfparser.authors[0]);
+            Drupal.settings.pdfparser.authors.splice(0, 1);
+            if (Drupal.settings.pdfparser.authors.length != 0) {
+              $('#edit-author-reference-und-add-more').trigger('mousedown');
+            }
+          }
+        }
       }
     }
   });
