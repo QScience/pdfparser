68059668837a08ef1a174461f5c1a7b11a295d51
kojak <tamas.kojedzinszky@gmail.com>
2012-12-04 15:25:43 +0100

    Adding citations automatically to paper.

--- js/pdfparser.js
+++ js/pdfparser.js
@@ -1,20 +1,10 @@
 (function ($) {
-  $(document).ready(function() {
-//    console.log('pdfparser loaded...');
-  });
-  
   $(document).ajaxComplete(function(e, xhr, settings) {
     console.log(settings);
     if (typeof settings.extraData != 'undefined') {
       var field_num;
+      var i, j;
       if (settings.extraData._triggering_element_name == 'field_paper_upload_und_0_upload_button') {
-  //      var num = $('#edit-field-paper-upload div.messages').length;
-  //      var i = 1;
-  //      $('#edit-field-paper-upload div.messages').each(function() {
-  //        if (i >= num) return;
-  //        $(this).slideUp(1000);
-  //        i++;
-  //      });
         if (Drupal.settings && Drupal.settings.pdfparser) {
           if (Drupal.settings.pdfparser.title) {
             $('#edit-title').val(Drupal.settings.pdfparser.title);
@@ -23,14 +13,9 @@
             $('#edit-abstract-und-0-value').val(Drupal.settings.pdfparser.abstr);
           }
           if (Drupal.settings.pdfparser.authors) {
-//            console.log(Drupal.settings.pdfparser.authors);
             field_num = $('#author-reference-values tbody input.form-text').length;
             var authors = Drupal.settings.pdfparser.authors;
             var author_num = authors.length;
-            var i, j;
-//            for (i = 0; i < author_num - field_num; i++) {
-//              $('#edit-author-reference-und-add-more').trigger('mousedown');
-//            }
             for (i = 0, j = 0; i < author_num; j++) {
               if (j >= field_num) {
                 break;
@@ -42,14 +27,22 @@
             if (Drupal.settings.pdfparser.authors.length != 0) {
               $('#edit-author-reference-und-add-more').trigger('mousedown');
             }
-//            console.log(Drupal.settings.pdfparser.authors);
-//            for (i = 0; i < author_num; i++) {
-//              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val(authors[i]);
-//            }
-//            for (i = author_num; i < field_num; i++) {
-//              $('#author-reference-values tbody #edit-author-reference-und-'+ i +'-target-id').val('');
-//            }
-            //$('#edit-authorname-und-0-value').val(Drupal.settings.pdfparser.authors);
+          }
+          if (Drupal.settings.pdfparser.citations) {
+            field_num = $('#citation-reference-values tbody input.form-text').length;
+            var citations = Drupal.settings.pdfparser.citations;
+            var citation_num = citations.length;
+            for (i = 0, j = 0; i < citation_num; j++) {
+              if (j >= field_num) {
+                break;
+              } else {
+                $('#citation-reference-values tbody #edit-citation-reference-und-'+ j +'-target-id').val(citations[i].title);
+                Drupal.settings.pdfparser.citations.splice(i, 1);
+              }
+            }
+            if (Drupal.settings.pdfparser.citations.length != 0) {
+              $('#edit-citation-reference-und-add-more').trigger('mousedown');
+            }
           }
         }
       } else if (settings.extraData._triggering_element_name == 'field_paper_upload_und_0_remove_button') {
@@ -65,6 +58,17 @@
             Drupal.settings.pdfparser.authors.splice(0, 1);
             if (Drupal.settings.pdfparser.authors.length != 0) {
               $('#edit-author-reference-und-add-more').trigger('mousedown');
+            }
+          }
+        }
+      } else if (settings.extraData._triggering_element_name == 'citation_reference_add_more') {
+        if (Drupal.settings && Drupal.settings.pdfparser) {
+          if (Drupal.settings.pdfparser.citations && Drupal.settings.pdfparser.citations.length > 0) {
+            field_num = $('#citation-reference-values tbody input.form-text').length;
+            $('#citation-reference-values tbody #edit-citation-reference-und-'+ (field_num - 1) +'-target-id').val(Drupal.settings.pdfparser.citations[0].title);
+            Drupal.settings.pdfparser.citations.splice(0, 1);
+            if (Drupal.settings.pdfparser.citations.length != 0) {
+              $('#edit-citation-reference-und-add-more').trigger('mousedown');
             }
           }
         }
